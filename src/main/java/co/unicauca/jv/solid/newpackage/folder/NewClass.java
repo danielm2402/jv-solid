c583bccd0713087d51b37150a093e21aa1cb1a09
danielm2402 <damunoz216@unicauca.edu.co>
2020-09-14 17:43:47 -0500

    test class

---
+++ src/main/java/co/unicauca/jv/solid/newpackage/folder/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.unicauca.jv.solid.newpackage.folder;
+
+/**
+ *
+ * @author daniel2402
+ */
+public class NewClass {
+    
+}
